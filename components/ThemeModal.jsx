3a4516dd926a7d98238e100fbfb49ef31dbbb882
Julien <julien7henry@gmail.com>
2022-08-29 13:32:01 +0200

    ThemeContext case modified

--- components/ThemeModal.jsx
+++ components/ThemeModal.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "../context/themeContext";
+import { ThemeContext } from "../context/ThemeContext";
 
 import { Menu } from '@headlessui/react'
 import { SunIcon } from '@heroicons/react/outline';
