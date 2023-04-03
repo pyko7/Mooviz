edd6016c192b708b3719b167fccac8f1a6384430
julien henry <julienhenry967@gmail.com>
2023-04-03 11:13:43 +0200

    chore: remove useless files

--- components/Loaders/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-  return (
-    <div className="w-full text-center">
-      <span className="relative inline-block w-12 h-12 border-4 border-white border-b-red-700 dark:border-neutral-300 dark:border-b-white rounded-[50%] animate-spin" />
-    </div>
-  );
-};
-
-export default LoadingSpinner;
